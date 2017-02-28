2b6a5c1a6c5f40b50da9c58d403bf4a3a9165968
Burrely <dinosammy2@gmail.com>
2017-02-28 22:35:00 +0100

    Music player updates/fixes
    - Added rainbow borders while playing videos.
    - Panel open button displays direction accordingly.
    - Play/Pause button will now say Play or Pause according to the video
    state.
    - Volume slider will dynamically change the volume while sliding instead
    of after mouse release.
    - Previous, Play/Pause and Next button have gotten a new style to make
    them look more interactable.

--- Assets/Scripts/Page system.js
+++ Assets/Scripts/Page system.js
@@ -10,22 +10,32 @@ $(window).on("load", function(){
 $(document).ready(function() {
 	
 	$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
+	switchMusicPanelSwitchText();
 	
-	setTimeout(function() {
+	var hidePanel = setTimeout(function() {
+		if (!$("div.youtubeMusicPanel").hasClass("youtubeMusicPanelActive")) {
+			return;
+		} //Stops the function in this timeOut method if the youtube music panel isn't active.
 		$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
+		switchMusicPanelSwitchText();
 	}, 3000);
 	
-	$('input[type="range" i].youtubeMusicVolumeSlider').change(function() {
+	$('input[type="range" i].youtubeMusicVolumeSlider').mousemove(function() {
 		
+		clearInterval(hidePanel);
 		player.setVolume($(this).val());
 		
 	});
 	
 	$('button.switchYoutubeMusicPanel').on("click", function() {
 		
+		clearInterval(hidePanel);
 		$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
 		
+		switchMusicPanelSwitchText();
+		
 		$(this).fadeTo("fast", 0.5, function() {
+
 			$(this).fadeTo("fast", 1);
 			
 		});
@@ -34,25 +44,38 @@ $(document).ready(function() {
 	
 	$('button.nextYoutubeMusicTrack').on("click", function() {
 		
+		startYoutubeRainbowBorders();
+		clearInterval(hidePanel);
 		player.nextVideo();
+		$('button.startYoutubeMusicTrack').text("Pause");
 		
 	});
 	
 	$('button.previousYoutubeMusicTrack').on("click", function() {
 		
+		startYoutubeRainbowBorders();
+		clearInterval(hidePanel);
 		player.previousVideo();
+		$('button.startYoutubeMusicTrack').text("Pause");
 		
 	});
 	
 	$('button.startYoutubeMusicTrack').on("click", function() {
 		
+		clearInterval(hidePanel);
+		startYoutubeRainbowBorders();
+		
 		if (player.getPlayerState() != 1 && player.getPlayerState() != 3) {
 			
+			$(this).text("Pause");
 			player.playVideo();
 			
 		} else {
 			
+			$(this).text("Play");
 			player.pauseVideo();
+			clearInterval(rainbowFrame);
+			$("div.youtubeMusicPanel").css({"border": "1px solid hsl(0, 100%,100%)", "border-left": "2px"});
 			
 		}
 		
@@ -79,7 +102,7 @@ function onYouTubeIframeAPIReady() {
 function onPlayerReady() {
 	
 	//Sets the volume to 1 (not bothersome).
-	player.setVolume(1);
+	player.setVolume(5);
 	//Shuffles playlist.
 	player.setShuffle(true); //Sets the youtube setting for shuffled playlists on for this playlist.
 	//Randomizes first video.
@@ -88,5 +111,39 @@ function onPlayerReady() {
 	player.playVideoAt(firstVideoInIndex); //Sets the playlist index but also starts playing it (look below).
 	player.stopVideo(); //Stops the video immediately to make sure no data will be used for no reason.
 	
-	$('input[type="range" i].youtubeMusicVolumeSlider').val(player.getVolume() / 100);
+	$('input[type="range" i].youtubeMusicVolumeSlider').val(player.getVolume() / 20);
+	
+	if (player.getPlayerState() != 1 && player.getPlayerState() != 3) {
+		$('button.startYoutubeMusicTrack').text("Play");
+	} else {
+		$('button.startYoutubeMusicTrack').text("Pause");
+	}
+}
+
+function switchMusicPanelSwitchText() {
+	if ($("div.youtubeMusicPanel").hasClass("youtubeMusicPanelActive")) {
+				
+		$("button.switchYoutubeMusicPanel").text("<|");
+		
+	} else {
+		
+		$("button.switchYoutubeMusicPanel").text("|>");
+		
+	}
+}
+
+var rainbowCounter = 0;
+var rainbowFrame;
+
+function startYoutubeRainbowBorders() {
+	clearInterval(rainbowFrame);
+	rainbowFrame = setInterval(function() {
+			rainbowCounter++;
+			if (rainbowCounter > 360) {
+				rainbowCounter = 0;
+			}
+			$("div.youtubeMusicPanel").css({"border": "1px solid hsl(" + String(rainbowCounter) + ", 100%,40%)", "border-left": "2px"});
+		},
+			1000/(24*4)
+	);
 }
