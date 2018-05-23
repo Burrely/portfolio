e0bb339f1a49b81b48f03d1c1aebbca35f41ae6e
Sammy Khachane <Burrely@users.noreply.github.com>
2018-05-23 14:03:57 +0200

    .

--- Assets/Scripts/Page system.js
+++ Assets/Scripts/Page system.js
@@ -15,7 +15,31 @@ $(window).on("load", function(){
 >>>>>>> parent of f59e795... Added button images
 	$(".pageLoadingscreen").fadeOut("slow", 0);
 });
+<<<<<<< HEAD
 =======
 });
 
 >>>>>>> parent of 46db1f1... Music player update
+=======
+
+//Youtube
+var usingYoutubePlayer = true;
+var player; //If using youtube player.
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("video", {
+	height: '390',
+	width: '640',
+	videoId: 'PLgvkpXZcWJIW5xBPwAGLw28mT78YDf-iE',
+	events: {
+	  'onReady': onPlayerReady,
+	  //'onStateChange': onPlayerStateChange
+	}
+  });
+}
+
+function onPlayerReady() {
+	player.setVolume(1);
+	player.setShuffle(true);
+}
+>>>>>>> parent of 2acaea5... Big music player overhaul!
