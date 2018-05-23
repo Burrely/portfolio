e0bf8d3445f22d1c2d049129b5cd8467a7f2e8ac
Sammy Khachane <Burrely@users.noreply.github.com>
2018-05-23 14:01:27 +0200

    .

--- Assets/Scripts/Page system.js
+++ Assets/Scripts/Page system.js
@@ -1,10 +1,16 @@
 //Jquery
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 $(document).ready( function(){
 =======
 $(window).ready( function(){
 >>>>>>> parent of f919251... Fix 2
 	console.log("Page has loaded");
+=======
+$(window).on("load", function(){
+	console.log("Page has loaded");
+	
+>>>>>>> parent of f59e795... Added button images
 	$(".pageLoadingscreen").fadeOut("slow", 0);
 });
