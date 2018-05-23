11ba63039f7f0c9535e51277cbb9a2071f1e0d35
Sammy Khachane <Burrely@users.noreply.github.com>
2018-05-23 14:15:54 +0200

    Attempt to fix this shit 1

--- Assets/Scripts/Page system.js
+++ Assets/Scripts/Page system.js
@@ -1,108 +1,95 @@
 //Jquery
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 $(document).ready( function(){
-=======
-$(window).ready( function(){
->>>>>>> parent of f919251... Fix 2
-	console.log("Page has loaded");
-=======
-=======
->>>>>>> parent of 005ca3d... Visuals update
-$(window).on("load", function(){
-	console.log("Page has loaded");
 	
-<<<<<<< HEAD
->>>>>>> parent of f59e795... Added button images
-	$(".pageLoadingscreen").fadeOut("slow", 0);
-});
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
+	$(window).ready( function(){
+		
+		console.log("Page has loaded");
 
-$(document).ready(function() {
-	
-	$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
-	
-	setTimeout(function() {
-		$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
-	}, 3000);
-	
-	$('input[type="range" i].youtubeMusicVolumeSlider').change(function() {
-		
-		player.setVolume($(this).val());
-		
 	});
 	
-	$('button.switchYoutubeMusicPanel').on("click", function() {
+	$(window).on("load", function(){
+		console.log("Page has loaded");
+		
+		$(".pageLoadingscreen").fadeOut("slow", 0);
+	});
+
+	$(document).ready(function() {
 		
 		$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
 		
-		$(this).fadeTo("fast", 0.5, function() {
-			$(this).fadeTo("fast", 1);
+		setTimeout(function() {
+			$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
+		}, 3000);
+		
+		$('input[type="range" i].youtubeMusicVolumeSlider').change(function() {
+			
+			player.setVolume($(this).val());
+			
+		});
+		
+		$('button.switchYoutubeMusicPanel').on("click", function() {
+			
+			$("div.youtubeMusicPanel").toggleClass("youtubeMusicPanelActive");
+			
+			$(this).fadeTo("fast", 0.5, function() {
+				$(this).fadeTo("fast", 1);
+				
+			});
+			
+		});
+		
+		$('button.nextYoutubeMusicTrack').on("click", function() {
+			
+			player.nextVideo();
+			
+		});
+		
+		$('button.previousYoutubeMusicTrack').on("click", function() {
+			
+			player.previousVideo();
+			
+		});
+		
+		$('button.startYoutubeMusicTrack').on("click", function() {
+			
+			if (player.getPlayerState() != 1 && player.getPlayerState() != 3) {
+				
+				player.playVideo();
+				
+			} else {
+				
+				player.pauseVideo();
+				
+			}
 			
 		});
 		
 	});
-	
-	$('button.nextYoutubeMusicTrack').on("click", function() {
-		
-		player.nextVideo();
-		
-	});
-	
-	$('button.previousYoutubeMusicTrack').on("click", function() {
-		
-		player.previousVideo();
-		
-	});
-	
-	$('button.startYoutubeMusicTrack').on("click", function() {
-		
-		if (player.getPlayerState() != 1 && player.getPlayerState() != 3) {
-			
-			player.playVideo();
-			
-		} else {
-			
-			player.pauseVideo();
-			
+
+
+	//Youtube
+	var usingYoutubePlayer = true;
+	var player; //If using youtube player.
+
+	function onYouTubeIframeAPIReady() {
+	player = new YT.Player("video", {
+		height: '390',
+		width: '640',
+		videoId: 'PLgvkpXZcWJIW5xBPwAGLw28mT78YDf-iE',
+		events: {
+		'onReady': onPlayerReady,
+		//'onStateChange': onPlayerStateChange
 		}
-		
 	});
-	
->>>>>>> parent of 2b6a5c1... Music player updates/fixes
-});
-
->>>>>>> parent of 46db1f1... Music player update
-=======
-
-//Youtube
-var usingYoutubePlayer = true;
-var player; //If using youtube player.
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("video", {
-	height: '390',
-	width: '640',
-	videoId: 'PLgvkpXZcWJIW5xBPwAGLw28mT78YDf-iE',
-	events: {
-	  'onReady': onPlayerReady,
-	  //'onStateChange': onPlayerStateChange
 	}
-  });
-}
 
-function onPlayerReady() {
-<<<<<<< HEAD
-	player.setVolume(1);
-	player.setShuffle(true);
-}
->>>>>>> parent of 2acaea5... Big music player overhaul!
-=======
+	function onPlayerReady() {
+		
+		player.setVolume(1);
+		player.setShuffle(true);
+	}
+
 	
 	//Sets the volume to 1 (not bothersome).
 	player.setVolume(1);
@@ -114,11 +101,8 @@ function onPlayerReady() {
 	player.playVideoAt(firstVideoInIndex); //Sets the playlist index but also starts playing it (look below).
 	player.stopVideo(); //Stops the video immediately to make sure no data will be used for no reason.
 	
-<<<<<<< HEAD
 	$('input[type="range" i].youtubeMusicVolumeSlider').val(player.getVolume() / 100);
-}
->>>>>>> parent of 2b6a5c1... Music player updates/fixes
-=======
+
 });
 
 $(document).ready(function() {
@@ -261,4 +245,3 @@ function startYoutubeRainbowBorders() {
 			1000/(24*4)
 	);
 }
->>>>>>> parent of 005ca3d... Visuals update
