9ea13427b1ae0b25b663d4f59a52ffb58662d0a4
Sammy Khachane <Burrely@users.noreply.github.com>
2018-06-11 02:02:19 +0200

    DFC timer
    That's it really.

--- Assets/Scripts/Page system.js
+++
@@ -1,9 +0,0 @@
-//Jquery
-
-//Loading screen.
-$(window).on("load", function(){
-	
-	console.log("Page has loaded");
-	$(".pageLoadingscreen").fadeOut("slow", 0);
-	
-});
---
+++ Assets/main.js
@@ -0,0 +1,28 @@
+
+var dayTimerContainer = document.getElementById("DayCounter");
+var timerContainer = document.getElementById("TimeCounter");
+var timer;
+var endTime = new Date(2018, 5, 29, 17, 0, 0, 0);
+
+function UpdateTimer() {
+    var timeNow = Date.now();
+    var remTimeMilliseconds = new Date((endTime.getTime() - timeNow)).getTime();
+    
+    //
+    var rDayInit = Math.floor(remTimeMilliseconds / 1000 / 60 / 60 / 24);
+    var rHourinit = Math.floor(remTimeMilliseconds / 1000 / 60 / 60) % 24;
+    var rMinuteinit = Math.floor(remTimeMilliseconds / 1000 / 60) % 60;
+    var rSecondinit = Math.floor(remTimeMilliseconds / 1000) % 60;
+
+    //Next values are strings
+    var remHours = rHourinit < 10 ? "0" + rHourinit.toString() : rHourinit.toString();
+    var remMinutes = rMinuteinit < 10 ? "0" + rMinuteinit.toString() : rMinuteinit.toString();
+    var remSecs = rSecondinit < 10 ? "0" + rSecondinit.toString() : rSecondinit.toString();
+
+    var finalTimeString = remHours + " : " + remMinutes + " : " + remSecs;
+
+    dayTimerContainer.innerHTML = rDayInit.toString();
+    timerContainer.innerHTML = finalTimeString;
+}
+
+timer = setInterval(UpdateTimer, 1000);
